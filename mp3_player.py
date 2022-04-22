e3121e728edea9f5df963482e640441f6e3f4e92
boyinblue <boyinblue@naver.com>
2022-04-23 06:48:38 +0900

    mp3 player

--- mp3_player.py
+++ mp3_player.py
@@ -12,20 +12,39 @@ def pipe_open():
     return os.fdopen(pipe_fd)
 
 def read_pipe(pipe):
-    return pipe.readline().strip()
+    return pipe.readline()
 
 def handle_pipe(player, message):
+#    print("msg :", message)
+    message = message.strip()
     print("Msg :", message, message[-4:])
     if message[-4:].lower() == ".mp3":
         print("queue")
-        player.queue(pyglet.media.load(message, streaming=False))
+        player.queue(pyglet.media.load(message))
+    elif message == "next":
+        print("next")
+        player.next_source()
 
 def handle_player(player):
+    if not player.playing:
+        print("Play!")
     player.play()
+
+def on_eos():
+    print("on_eos()")
+
+def on_player_eos():
+    print("on_player_eos()")
+
+def on_player_next_source():
+    print("on_player_next_source()")
 
 def main():
     pipe = pipe_open()
     player = pyglet.media.Player()
+    player.on_eos = on_eos
+    player.on_player_eos = on_player_eos
+    player.on_player_next_source = on_player_next_source
 
     try:
         while True:
